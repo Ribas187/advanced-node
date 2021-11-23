828baacae265fd2b487cd9722bf755a5fe8d5e8e
Guilherme Ribas <guilherme.ribas.tech@gmail.com>
2021-11-23 16:44:23 -0300

    test: create sut factory method

--- tests/data/use-cases/facebook-authentication.spec.ts
+++ tests/data/use-cases/facebook-authentication.spec.ts
@@ -1,27 +1,40 @@
 /* eslint-disable max-classes-per-file */
-import { mock } from 'jest-mock-extended';
+import { mock, MockProxy } from 'jest-mock-extended';
 import { FacebookAuthenticationUseCase } from '@/data/use-cases';
 import { AuthenticationError } from '@/domain/errors';
 import { LoadFacebookUserApi } from '@/data/contracts/apis';
 
+type SutTypes = {
+  sut: FacebookAuthenticationUseCase;
+  loadFacebookUserApi: MockProxy<LoadFacebookUserApi>;
+};
+
+const makeSut = (): SutTypes => {
+  const loadFacebookUserApi = mock<LoadFacebookUserApi>();
+  const sut = new FacebookAuthenticationUseCase(loadFacebookUserApi);
+
+  return {
+    sut,
+    loadFacebookUserApi,
+  };
+};
+
 describe('FacebookAuthenticationUseCase', () => {
   it('should call LoadFacebookUserApi with correct params', async () => {
-    const loadFacebookUserApi = mock<LoadFacebookUserApi>();
-
-    const sut = new FacebookAuthenticationUseCase(loadFacebookUserApi);
+    const { sut, loadFacebookUserApi } = makeSut();
 
     await sut.perform({ token: 'dummy_token' });
 
-    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'dummy_token' });
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({
+      token: 'dummy_token',
+    });
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1);
   });
 
   it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
-    const loadFacebookUserApi = mock<LoadFacebookUserApi>();
+    const { sut, loadFacebookUserApi } = makeSut();
 
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined);
-
-    const sut = new FacebookAuthenticationUseCase(loadFacebookUserApi);
 
     const authResult = await sut.perform({ token: 'dummy_token' });
 
