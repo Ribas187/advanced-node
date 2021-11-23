ea5420c8d5fc5d56d12cbed0148299a58c9d931d
Guilherme Ribas <guilherme.ribas.tech@gmail.com>
2021-11-23 16:47:00 -0300

    test: change sut strategy to beforeEach

--- tests/data/use-cases/facebook-authentication.spec.ts
+++ tests/data/use-cases/facebook-authentication.spec.ts
@@ -4,39 +4,29 @@ import { FacebookAuthenticationUseCase } from '@/data/use-cases';
 import { AuthenticationError } from '@/domain/errors';
 import { LoadFacebookUserApi } from '@/data/contracts/apis';
 
-type SutTypes = {
-  sut: FacebookAuthenticationUseCase;
-  loadFacebookUserApi: MockProxy<LoadFacebookUserApi>;
-};
-
-const makeSut = (): SutTypes => {
-  const loadFacebookUserApi = mock<LoadFacebookUserApi>();
-  const sut = new FacebookAuthenticationUseCase(loadFacebookUserApi);
-
-  return {
-    sut,
-    loadFacebookUserApi,
-  };
-};
-
 describe('FacebookAuthenticationUseCase', () => {
-  it('should call LoadFacebookUserApi with correct params', async () => {
-    const { sut, loadFacebookUserApi } = makeSut();
+  let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>;
+  let sut: FacebookAuthenticationUseCase;
+  const token = 'dummy_token';
 
-    await sut.perform({ token: 'dummy_token' });
+  beforeEach(() => {
+    loadFacebookUserApi = mock();
+    sut = new FacebookAuthenticationUseCase(loadFacebookUserApi);
+  });
+
+  it('should call LoadFacebookUserApi with correct params', async () => {
+    await sut.perform({ token });
 
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({
-      token: 'dummy_token',
+      token,
     });
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1);
   });
 
   it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
-    const { sut, loadFacebookUserApi } = makeSut();
-
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined);
 
-    const authResult = await sut.perform({ token: 'dummy_token' });
+    const authResult = await sut.perform({ token });
 
     expect(authResult).toEqual(new AuthenticationError());
   });
